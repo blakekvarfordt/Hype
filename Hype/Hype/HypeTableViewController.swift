8a94aa161f7da15a63f219c60aedacb4ebbdd9db
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-26 12:38:13 -0600

    final commit with table view controller etc.

--- Hype/Hype/HypeTableViewController.swift
+++ Hype/Hype/HypeTableViewController.swift
@@ -12,27 +12,71 @@ class HypeTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
     }
 
+    func loadData() {
+        HypeController.shared.fetchDemHypes { (success) in
+            if success {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    func addAlert() {
+        // create alert controller
+        let alertController = UIAlertController(title: "Get Hype", message: "Hype is up sir!", preferredStyle: .alert)
+        // add text field to controller
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Yeah Buddy"
+        }
+        // create save action button for controller
+        let hypeAction = UIAlertAction(title: "Add Hype", style: .default) { (_) in
+            // get text from text field
+            guard let text = alertController.textFields?.first?.text else { return }
+            // actually add hype if there is text
+            if text != ""{
+                // This is a closure so it will be on a background thread
+                HypeController.shared.saveHype(with: text, completion: { (success) in
+                    if success {
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                    }
+                })
+            }
+        }
+        // set up cancelation action button on alert controller
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        // add actions to controller
+        alertController.addAction(hypeAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+    
+    
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return HypeController.shared.hypes.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hypeCell", for: indexPath)
+        let hype = HypeController.shared.hypes[indexPath.row]
         
+        cell.textLabel?.text = hype.text
+        cell.detailTextLabel?.text = "\(hype.timestamp)"
 
         return cell
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        addAlert()
     }
 
 }
